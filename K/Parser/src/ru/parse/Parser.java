b979a8e546f2e7a24e6756ae330e2a55fd02160b
TheRealOkone <70758410+TheRealOkone@users.noreply.github.com>
2020-12-22 20:45:35 +0300

    Ready

--- K/Parser/src/ru/parse/Parser.java
+++ K/Parser/src/ru/parse/Parser.java
@@ -1,12 +1,60 @@
 package ru.parse;
 
+import java.io.File;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Parser {
     static {
         System.loadLibrary("TNParser");
     }
+    private static ArrayBlockingQueue<File> qjpg;
+    private static ArrayBlockingQueue<File> qtxt;
+    public void checkout(){
+        int i;
+        ArrayBlockingQueue<File> dropjpg = new ArrayBlockingQueue<File>(500);
+        ArrayBlockingQueue<File> droptxt = new ArrayBlockingQueue<File>(500);
+        for(i=0;i<500;i++){
+            File filejpg = new File("KZ", "image" + String.valueOf(i) +".jpg");
+            File filetxt = new File("KZ", "request" + String.valueOf(i) +".txt");
+            if(filejpg.isFile() && filetxt.isFile()){
+                dropjpg.add(filejpg);
+                droptxt.add(filetxt);
+            }
+        }
+        qjpg = dropjpg;
+        qtxt = droptxt;
+
+    }
+
+    public static ArrayBlockingQueue<File> getQjpg() {
+        return qjpg;
+    }
+
+    public static ArrayBlockingQueue<File> getQtxt() {
+        return qtxt;
+    }
+
     static native void activate();
     static native void deactivate();
-    public static void main(String[] argv){
-        activate();
+    public void Parser(){
+        Runnable task1 = new Runnable() {
+            public void run() {
+                activate();
+            }
+        };
+        Runnable task2 = new Runnable() {
+            public void run() {
+                while (true) {
+                    checkout();
+                }
+            }
+        };
+        Thread thread1 = new Thread(task1);
+        thread1.start();
+        Thread thread2 = new Thread(task2);
+        thread2.start();
+
     }
+
+
 }
